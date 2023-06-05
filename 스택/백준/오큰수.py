89ca2fd54683d47033345967775e51fcb334ef83
hee2425 <bona366@naver.com>
2023-06-05 16:07:03 +0900

    골드4

---
+++ 스택/백준/오큰수.py
@@ -0,0 +1,13 @@
+import sys
+n = int(input())
+arr = list(map(int, sys.stdin.readline().split()))
+stack = []
+answer = [-1]*n
+
+stack.append(0)
+for i in range(1, n):
+    while stack and arr[stack[-1]]<arr[i]:
+        answer[stack.pop()] = arr[i]
+    stack.append(i)
+
+print(*answer)
