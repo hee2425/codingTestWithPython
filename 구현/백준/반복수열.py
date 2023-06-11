ccbe88c62a2616cc133e6d228df33c3e70c60cd2
hee2425 <bona366@naver.com>
2023-06-11 16:45:19 +0900

    실버 4

---
+++ 구현/백준/반복수열.py
@@ -0,0 +1,14 @@
+a, p = map(int, input().split())
+dlist = [a]
+
+def square_p(x):
+    x = int(x) ** p
+    return x
+
+while True:
+    num = sum(map(square_p, str(dlist[-1])))
+    if num in dlist:
+        break
+    dlist.append(num)
+
+print(dlist.index(num))  #같은 숫자의 인덱스를 찾는다
