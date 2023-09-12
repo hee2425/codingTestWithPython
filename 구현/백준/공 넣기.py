e98ab704580aa7e132cbe53bf7fcf5584118b384
hee2425 <bona366@naver.com>
2023-09-12 18:27:08 +0900

    브론즈 3

---
+++ 구현/백준/공 넣기.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+
+basket = [0]*N
+
+for i in range(M):
+    i,j,k = map(int, input().split())
+
+    for n in range(i,j+1):
+        basket[n-1] = k
+
+print(*basket)
